06db38f05170b0bd9d5262d023dc11538e90c871
Hackari <simreryan@gmail.com>
2025-06-08 15:16:30 +0800

    Add oswp.py containing exam information

---
+++ OSWP/oswp.py
@@ -0,0 +1,9 @@
+KALI_IP = 192.168.40.117
+
+
+def examInfo() {
+  examRequirements = "There are three network scenarios. For each scenario, obtain a wireless key, connect to the target Access Point (AP), and obtain the proof file from http://IP/proof.txt. Complete and obtain proof.txt on two network scenarios, with one being mandatory to complete."
+  submission = "TODO"
+  print(examRequirements)
+  print(submission)
+}
